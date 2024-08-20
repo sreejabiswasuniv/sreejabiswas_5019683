9b4985e1cff1669b67169479fd5902f326693571
Sreeja Biswas <13000121082.cse.tmsl@ticollege.org>
2024-08-21 00:53:01 +0530

    Delete BookstoreAPIEx5 directory

--- BookstoreAPIEx5/src/main/java/com/example/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
