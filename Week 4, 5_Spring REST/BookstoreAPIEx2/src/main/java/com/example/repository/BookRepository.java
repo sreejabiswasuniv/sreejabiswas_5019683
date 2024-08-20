dd8d870bf142c2c5034c738ec880973f13df7f4f
Sreeja Biswas <13000121082.cse.tmsl@ticollege.org>
2024-08-21 00:54:05 +0530

    Delete Week 4, 5_Spring REST directory

--- Week 4, 5_Spring REST/BookstoreAPIEx2/src/main/java/com/example/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
